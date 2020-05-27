554e4d52ed995fa06d1fd582a80f5f5be91cadbe
Sixxxty <48228517+Sixxxty@users.noreply.github.com>
2020-05-27 15:42:29 -0300

    ProcFile

---
+++ rankYoda.js
@@ -0,0 +1,19 @@
+var request = require('request')
+var cheerio = require('cheerio')
+let ranks = [];
+request('https://shadowarena.pearlabyss.com/en-US/Arena?battleType=0&server=sa',function( err, res, body){
+	if(err) console.log('Erro: ' + err);
+	var $ = cheerio.load(body);
+	$('.box_list_area').each(function(){
+		var name = $(this).find('.thum_name').text()
+		ranks.push(name)
+
+	});
+	for(var i = 0; i <ranks.length; i++){
+		if(ranks[i] === "YoDaSL"){
+			console.log("YoDaSL está atualmente top:", i+1,"do servidor SA")
+			break
+		}
+	}
+
+});
